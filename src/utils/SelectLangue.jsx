25a102c53fe484788609e3f31989903684b7aa2e
kiadyabel <randri12abel@gmain.com>
2023-09-13 08:22:10 +0200

    paramaetre langue

--- src/utils/SelectLangue.jsx
+++ src/utils/SelectLangue.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import i18n from "../TranslationI18n";
 import { Select, MenuItem } from "@mui/material";
 //import { useTranslation } from "react-i18next";
-import FlagFr from "../utils/image/france.png"
+import FlagFr from "../utils/image/france.png";
 import FlagEn from "../utils/image/anglais.png";
 
 const LanguageSwitcher = () => {
-
- // const {i18n}=useTranslation() //pour changer la langue
+  // const {i18n}=useTranslation() //pour changer la langue
   const onChangeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
@@ -19,17 +18,39 @@ const LanguageSwitcher = () => {
           background: "tranparent",
           height: "30px",
           color: "white",
+          fontSize: "12px",
           border: "none",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           ml: 3,
         }}
         defaultValue={i18n.language}
         onChange={(e) => onChangeLanguage(e.target.value)}
       >
-        <MenuItem value="fr">
-          <img src={FlagFr} alt="fr" style={{ alignItems: "center" }} /> FR
+        <MenuItem
+          value="fr"
+          sx={{
+            fontSize: "12px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "3px",
+          }}
+        >
+          <img src={FlagFr} alt="fr" /> <span>FR</span>
         </MenuItem>
-        <MenuItem value="en">
-          <img src={FlagEn} alt="en" style={{ alignItems: "center" }} /> EN
+        <MenuItem
+          value="en"
+          sx={{
+            fontSize: "12px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "3px",
+          }}
+        >
+          <img src={FlagEn} alt="en" /> <span>EN</span>
         </MenuItem>
         {/* Ajoutez d'autres langues ici avec leurs icônes */}
       </Select>
